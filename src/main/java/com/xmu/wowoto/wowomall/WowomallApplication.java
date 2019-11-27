54a1412c470fac4787ceced3f23d4325a4eab935
medalwill <42199368+medalwill@users.noreply.github.com>
2019-11-27 20:19:58 +0800

    1
    1

---
+++ src/main/java/com/xmu/wowoto/wowomall/WowomallApplication.java
@@ -0,0 +1,13 @@
+package com.xmu.wowoto.wowomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowomallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WowomallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmu/wowoto/wowomall/WowomallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmu.wowoto.wowomall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowomallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
