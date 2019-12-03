857a2838be955d0855564a4b555e9c111cb3cbad
fringe-k <1151712307@qq.com>
2019-12-03 20:49:48 +0800

    K's commit

--- src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
+++ src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
@@ -1,4 +1,14 @@
 package com.xmu.wowoto.wowomall.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@Api(value="Order",tags = "订单")
+@RestController
 public class OrderController {
+
 }
