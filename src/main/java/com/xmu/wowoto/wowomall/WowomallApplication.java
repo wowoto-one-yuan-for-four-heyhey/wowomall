202dcd4c15a2eafe0252e8fc013d746c7b4bde1a
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-08 11:54:10 +0800

    1

--- src/main/java/com/xmu/wowoto/wowomall/WowomallApplication.java
+++ src/main/java/com/xmu/wowoto/wowomall/WowomallApplication.java
@@ -1,15 +1,15 @@
 package com.xmu.wowoto.wowomall;
 
 
+import com.xmu.wowoto.wowomall.util.RedisUtil;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.xmu.wowoto.wowomall.mapper")
-
 public class WowomallApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WowomallApplication.class, args);
     }
