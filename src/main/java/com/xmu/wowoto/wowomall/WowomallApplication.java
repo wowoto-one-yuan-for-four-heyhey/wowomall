77f39ef76a2a635adf106310305cb67829e42fe1
Tens <tens444@163.com>
2019-12-11 14:46:43 +0800

    能跑

--- src/main/java/com/xmu/wowoto/wowomall/WowomallApplication.java
+++ src/main/java/com/xmu/wowoto/wowomall/WowomallApplication.java
@@ -6,9 +6,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.xmu.wowoto.wowomall.mapper")
+@EnableEurekaClient
 public class WowomallApplication {
     public static void main(String[] args) {
         SpringApplication.run(WowomallApplication.class, args);
--- src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
+++ src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
@@ -15,7 +15,6 @@ import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -229,7 +228,4 @@ public class OrderController {
 
         return orderService.comment(userId, Integer.parseInt(orderId));
     }
-
-
-
 }
