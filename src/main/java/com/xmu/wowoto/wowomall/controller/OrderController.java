a382a8cd213d9553a8805139e221df0fa60830d5
Tens <tens444@163.com>
2019-12-21 20:58:45 +0800

    能跑

--- src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
+++ src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
@@ -236,8 +236,8 @@ public class OrderController {
                 ResponseCode.ORDER_STATUS_CHANGE_FAILED.getMessage());
         }
         if(!order.getStatusCode().equals(Order.StatusCode.NOT_PAYED.getValue())){
-            return ResponseUtil.fail(ResponseCode.ORDER_STATUS_CHANGE_FAILED.getCode(),
-                            ResponseCode.ORDER_STATUS_CHANGE_FAILED.getMessage());}
+            return ResponseUtil.fail(ResponseCode.ORDER_INVAILD_OPERATION.getCode(),
+                            ResponseCode.ORDER_INVAILD_OPERATION.getMessage());}
         order = orderService.cancelOrder(order);
         return ResponseUtil.ok(order);
     }
