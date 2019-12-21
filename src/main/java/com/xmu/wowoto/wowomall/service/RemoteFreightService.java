a93c9ac55c3af7ebbb5caa53abbe4749e7cbe3f3
Tens <tens444@163.com>
2019-12-21 15:53:39 +0800

    能跑

--- src/main/java/com/xmu/wowoto/wowomall/service/RemoteFreightService.java
+++ src/main/java/com/xmu/wowoto/wowomall/service/RemoteFreightService.java
@@ -3,9 +3,7 @@ package com.xmu.wowoto.wowomall.service;
 import com.xmu.wowoto.wowomall.domain.Order;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -18,6 +16,6 @@ import java.math.BigDecimal;
 @FeignClient("freightService")
 public interface RemoteFreightService {
 
-    @GetMapping("/freightPrice")
-    String caculateFreight(@RequestBody Order order);
+    @PostMapping("/freightPrice")
+    String caculateFreight(Order order);
 }
