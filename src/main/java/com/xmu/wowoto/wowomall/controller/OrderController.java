04d5959b7bd4b03baa8d0ffd7a6e4c5e8836a1df
Tens <tens444@163.com>
2019-12-22 19:29:30 +0800

    能跑

--- src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
+++ src/main/java/com/xmu/wowoto/wowomall/controller/OrderController.java
@@ -210,7 +210,7 @@ public class OrderController {
         order = orderService.submit(order, cartItems);
 
         if(null == order)
-            ResponseUtil.fail(ORDER_SUBMIT_FAILED.getCode(), ORDER_SUBMIT_FAILED.getMessage());
+            return ResponseUtil.fail(ORDER_SUBMIT_FAILED.getCode(), ORDER_SUBMIT_FAILED.getMessage());
         return ResponseUtil.ok(order);
     }
 
